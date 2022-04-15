2317fae43fbf7eada07c4c86becbee67e8305aba
RyoNinoimya <cerezo0804@gmail.com>
2022-04-15 22:16:04 +0900

    XXXXX.phpを更新しました

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,11 +15,14 @@
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
-                <div class='posts'>
+                <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
