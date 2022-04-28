f21c5006a0c9f2fd0704ec33a62b44c61253217a
RyoNinoimya <cerezo0804@gmail.com>
2022-04-28 13:18:18 +0900

    コミットしまーす

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+    'title',
+    'body',
+    ];
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+        return $this->orderBy('id', 'DESC')->paginate($limit_count);
+    }
 }
